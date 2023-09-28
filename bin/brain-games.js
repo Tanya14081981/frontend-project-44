9110ea46f4a9f011b01fb1e205b23a11595a978f
Татьяна Андреева <tan-andreeva@mail.ru>
2023-09-28 17:39:28 +0300

    add  bin/brain-games.js src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
-import { name } from 'src/cli.js'
+import { name } from '../src/cli.js'
 
 name();
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
+import readlineSync from 'readline-sync'; 
+
 console.log('Welcome to the Brain Games!');
 
 export const name = readlineSync.question('May I have your name?');
-console.log('Hello ' + name + '!');
+console.log('Hello, ' + name + '!');
