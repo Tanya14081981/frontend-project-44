929d405763eff7cb4aec856eea03531d6dd941ba
Татьяна Андреева <tan-andreeva@mail.ru>
2023-09-29 22:33:10 +0300

    .eslintrc.yml  Makefile bin/brain-games.js package-lock.json package.json src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { greetGamer } from '../src/cli.js'
+import greetGamer from '../src/cli.js';
 
 greetGamer();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
-import readlineSync from 'readline-sync'; 
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-export const greetGamer = () => {
-const name = readlineSync.question('May I have your name?');
-console.log('Hello, ' + name + '!')
+const greetGamer = () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
 };
+export default greetGamer;
