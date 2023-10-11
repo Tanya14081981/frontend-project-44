8adceb00f0f19ab7a7b58dad86a246427e71c4d2
Татьяна Андреева <tan-andreeva@mail.ru>
2023-10-11 21:57:26 +0300

    a

--- src/calc.js
+++ src/calc.js
@@ -2,31 +2,39 @@ import readlineSync from 'readline-sync';
 
 import greetGamer from './cli.js';
 
+import getRandomInt from '../function/getRandomInt.js';
+
 const isRightAnswer = (numberOne, numberTwo, operator) => {
+  let result;
   switch (operator) {
     case '+':
-      return numberOne + numberTwo;
+      result = numberOne + numberTwo;
+      break;
     case '-':
-      return numberOne - numberTwo;
+      result = numberOne - numberTwo;
+      break;
     case '*':
-      return numberOne * numberTwo;
+      result = numberOne * numberTwo;
+      break;
     default:
       break;
   }
+  return result;
 };
 
 const calcGame = () => {
+  const userName = greetGamer();
+  console.log('What is the result of the expression?');
   for (let i = 0; i < 3; i += 1) {
-    const userName = greetGamer();
-    console.log('What is the result of the expression?');
-    const numberOne = Math.floor(Math.random() * 100);
-    const numberTwo = Math.floor(Math.random() * 100);
-    const operator = '+' || '-' || '*';
+    const numberOne = getRandomInt(100);
+    const numberTwo = getRandomInt(100);
+    const operators = ['+', '-', '*'];
+    const operator = operators[getRandomInt(3)];
     const expression = `${numberOne} ${operator} ${numberTwo}`;
     console.log('Question: ', expression);
     const answerCorrect = isRightAnswer(numberOne, numberTwo, operator);
     const answerUser = readlineSync.question('Your answer: ');
-    if (answerUser !== answerCorrect) {
+    if (answerUser !== answerCorrect.toString()) {
       console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerCorrect}'.`);
       console.log("Let's try again, ", userName);
       return;
